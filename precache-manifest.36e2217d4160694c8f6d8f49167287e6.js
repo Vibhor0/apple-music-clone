c008d3196ea0e59a8a180bc788ab591a082dd000
Vibhor0 <78290877+Vibhor0@users.noreply.github.com>
2022-02-22 12:28:32 +0530

    added files

---
+++ precache-manifest.36e2217d4160694c8f6d8f49167287e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e372f330fe487d89108",
+    "url": "/music/static/js/main.5c8c1971.chunk.js"
+  },
+  {
+    "revision": "3321ad820ee41c1cac90",
+    "url": "/music/static/js/runtime~main.4a84191b.js"
+  },
+  {
+    "revision": "d7cf5ef1096c8cff5ede",
+    "url": "/music/static/css/2.8fc3617c.chunk.css"
+  },
+  {
+    "revision": "d7cf5ef1096c8cff5ede",
+    "url": "/music/static/js/2.7a9d9c22.chunk.js"
+  },
+  {
+    "revision": "e5cbc3ad2163330c2fcbb98ea2a6244f",
+    "url": "/music/index.html"
+  }
+];
